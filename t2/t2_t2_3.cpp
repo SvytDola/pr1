0a9abdc1577432a44d8d7102cd7ecf7ed7eaf16f
Danila Abdullin <danila_abdullin@rntgroup.com>
2023-12-02 11:44:20 +0400

    Добавил ленточный алгоритм

--- t2/t2_t2_3.cpp
+++ t2/t2_t2_3.cpp
@@ -31,14 +31,6 @@ int main() {
     cout << "Матрица 2:" << endl;
     print_matrix(matrix2);
 
-    double start_time = omp_get_wtime();
-    const vector<vector<int>> &multiplication = matrix_multiplication(matrix1, matrix2);
-    cout << "Полученная матрица после перемножения матриц 1 и 2:" << endl;
-    print_matrix(multiplication);
-    double end_time = omp_get_wtime();
-    double execution_time = end_time - start_time; // Вычисление времени выполнения
-
-
     double start_time2 = omp_get_wtime();
     const vector<vector<int>> &multiplication2 = matrix_multiplication_parallel(matrix1, matrix2);
     cout << "Полученная матрица после перемножения матриц 1 и 2:" << endl;
@@ -47,7 +39,6 @@ int main() {
     double execution_time2 = end_time2 - start_time2; // Вычисление времени выполнения
 
 
-    std::cout << "Время выполнения в однопоточном режиме: " << execution_time << " секунд" << std::endl;
     std::cout << "Время выполнения в многопоточном режиме: " << execution_time2 << " секунд" << std::endl;
     return 0;
 }
@@ -69,23 +60,25 @@ vector<vector<int>> create_random_matrix(int size) {
     vector<vector<int>> matrix(size, vector<int>(size)); // Создание матрицы заданных размеров
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            matrix[i][j] = rand() % 100; // Генерация случайного числа от 0 до 99 и запись в ячейку матрицы
+            matrix[i][j] = rand() % 10; // Генерация случайного числа от 0 до 99 и запись в ячейку матрицы
         }
     }
     return matrix;
 }
 
 
-vector<vector<int>> matrix_multiplication(const vector<vector<int>> &matrix1, const vector<vector<int>> &matrix2) {
+vector<vector<int>> matrix_multiplication_parallel(const vector<vector<int>> &matrix1, const vector<vector<int>> &matrix2) {
     int m1 = matrix1.size();
     int n1 = matrix1[0].size();
     int n2 = matrix2[0].size();
 
     vector<vector<int>> result(m1, vector<int>(n2, 0));
 
+#pragma omp parallel for shared(matrix1, matrix2, result) schedule(static) num_threads(4)
     for (int i = 0; i < m1; i++) {
-        for (int j = 0; j < n2; j++) {
-            for (int k = 0; k < n1; k++) {
+        for (int j = 0; j < m1; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < m1; k++) {
                 result[i][j] += matrix1[i][k] * matrix2[k][j];
             }
         }
@@ -94,39 +87,3 @@ vector<vector<int>> matrix_multiplication(const vector<vector<int>> &matrix1, co
     return result;
 }
 
-
-vector<vector<int>>
-matrix_multiplication_parallel(const vector<vector<int>> &matrix1, const vector<vector<int>> &matrix2) {
-    int N = matrix1.size();
-    int n1 = matrix1[0].size();
-    int n2 = matrix2[0].size();
-    vector<vector<int>> result(N, vector<int>(n2, 0));
-    int num_threads = omp_get_max_threads(); // Количество потоков
-
-    if (omp_get_thread_num() % 2 == 0) {
-#pragma omp parallel for num_threads(num_threads)
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < N; i++) {
-                result[i][j] = 0;
-                for (int k = 0; k < N; k++) {
-                    result[i][j] += matrix1[i][k] * matrix2[k][j];
-                }
-            }
-            std::cout << "Поток " << omp_get_thread_num() << " выполнил столбец " << j << std::endl;
-        }
-    }
-
-        // Параллельное выполнение по строкам для студентов с нечетными номерами
-    else {
-#pragma omp parallel for num_threads(num_threads)
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
-                    result[i][j] += matrix1[i][k] * matrix2[k][j];
-                }
-            }
-            std::cout << "Поток " << omp_get_thread_num() << " выполнил строку " << i << std::endl;
-        }
-    }
-    return result;
-}
